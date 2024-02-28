f01d797ed3b75075f886eaf5e3d87b75e4829159
thexeromin <thexeromin@gmail.com>
2024-02-28 10:59:21 +0530

    update: display time in HH:MM:SS

--- pomo/library.c
+++ pomo/library.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <time.h>
 
+typedef struct {
+    int hour;
+    int minute;
+    int second;
+} TimeVal;
+
 static const time_t MAX_FOCUS_TIME = 1500;
 static const time_t MAX_BREAK_TIME = 300;
 
@@ -14,14 +20,23 @@ static void enable_cursor() {
     printf("\e[?25h");
 }
 
+// convert seconds to HH:MM:SS
+static void convert(time_t seconds, TimeVal* t) {
+    t->hour = seconds / 3600;
+    t->minute = (seconds / 60) % 60;
+    t->second = seconds % 60;
+}
+
 static void time_loop(time_t max, char* msg) {
     time_t curr_time = time(0);
     time_t start_time = time(0);
+    TimeVal t;
     
     disable_cursor();
     while(1) {
         time(&curr_time);
-        printf("%2ld\r", curr_time - start_time);
+        convert((curr_time - start_time), &t);
+        printf("%02d:%02d:%02d\r", t.hour, t.minute, t.second);
         if((curr_time - start_time) >= max) break;
     }
 
@@ -55,4 +70,3 @@ void init_help() {
 void cleanup() {
     enable_cursor();
 }
-
