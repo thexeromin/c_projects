176b445f407e463014de77d2ad53a1d5e46e2e47
thexeromin <thexeromin@gmail.com>
2024-03-05 10:22:04 +0530

    update: names of variables; add: finish message

--- pomo/screen_break.c
+++ pomo/screen_break.c
@@ -1,42 +1,58 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "raylib.h"
 #include "library.h"
 #include "screens.h"
 
 // Module Variables Definition (local)
-static Timer focusTimer = { 0 };
+static Timer timer = { 0 };
 static float maxBreakTime = 300;
 static char value[50];
+static bool isFinished = false;
+static char* msg = "Time to focus!";
 
 // Break Screen Initialization logic
 void InitBreakScreen(void) {
-    StartTimer(&focusTimer, maxBreakTime);
+    StartTimer(&timer, maxBreakTime);
 }
 
 // Break Screen Update logic
 void UpdateBreakScreen(void) {
-    UpdateTimer(&focusTimer);
-    sprintf(
-        value,
-        "%02.0f:%02d:%02d",
-        focusTimer.lifetime / 3600,
-        (int) (focusTimer.lifetime / 60) % 60,
-        (int) focusTimer.lifetime % 60
-    );
+    if(timer.lifetime <= 0) {
+        isFinished = true;
+    } else {
+        UpdateTimer(&timer);
+        sprintf(
+            value,
+            "%02.0f:%02d:%02d",
+            timer.lifetime / 3600,
+            (int) (timer.lifetime / 60) % 60,
+            (int) timer.lifetime % 60
+        );
+    }
 }
 
 // Break Screen Draw logic
 void DrawBreakScreen(void) {
     ClearBackground((Color) {56, 133, 138, 0});
 
-    DrawText(
-        value,
-        (GetScreenWidth() / 2) - 100,
-        100,
-        65,
-        WHITE
-    );
+    if(!isFinished)
+        DrawText(
+            value,
+            (GetScreenWidth() / 2) - 100,
+            100,
+            65,
+            WHITE
+        );
+    else
+        DrawText(
+            msg,
+            (GetScreenWidth() - strlen(msg)) / 2 - 100,
+            100,
+            30,
+            RAYWHITE
+        );
 }
 
 // Break Screen Unload logic
--- pomo/screen_focus.c
+++ pomo/screen_focus.c
@@ -1,42 +1,58 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "raylib.h"
 #include "library.h"
 #include "screens.h"
 
 // Module Variables Definition (local)
-static Timer focusTimer = { 0 };
-static float maxFocusTime = 1800;
+static Timer timer = { 0 };
+static float maxFocusTime = 1500;
 static char value[50];
+static bool isFinished = false;
+static char* msg = "Time for a break!";
 
 // Focus Screen Initialization logic
 void InitFocusScreen(void) {
-    StartTimer(&focusTimer, maxFocusTime);
+    StartTimer(&timer, maxFocusTime);
 }
 
 // Focus Screen Update logic
 void UpdateFocusScreen(void) {
-    UpdateTimer(&focusTimer);
-    sprintf(
-        value,
-        "%02.0f:%02d:%02d",
-        focusTimer.lifetime / 3600,
-        (int) (focusTimer.lifetime / 60) % 60,
-        (int) focusTimer.lifetime % 60
-    );
+    if(timer.lifetime <= 0) {
+        isFinished = true;
+    } else {
+        UpdateTimer(&timer);
+        sprintf(
+            value,
+            "%02.0f:%02d:%02d",
+            timer.lifetime / 3600,
+            (int) (timer.lifetime / 60) % 60,
+            (int) timer.lifetime % 60
+        );
+    }
 }
 
 // Focus Screen Draw logic
 void DrawFocusScreen(void) {
     ClearBackground((Color) {186, 73, 73, 0});
 
-    DrawText(
-        value,
-        (GetScreenWidth() / 2) - 100,
-        100,
-        65,
-        WHITE
-    );
+    if(!isFinished)
+        DrawText(
+            value,
+            (GetScreenWidth() / 2) - 100,
+            100,
+            70,
+            WHITE
+        );
+    else
+        DrawText(
+            msg,
+            (GetScreenWidth() - strlen(msg)) / 2 - 100,
+            100,
+            30,
+            RAYWHITE
+        );
 }
 
 // Focus Screen Unload logic
